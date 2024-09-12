5cd97b08c680602251d4aa99eb3df4a34145cb70
OkechukwuMiracle <damianmiracle18@gmail.com>
2024-09-12 21:32:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// FLOATING LABEL EFFECT
+var input = $('.md-input')
+input.on('blur',function(){
+    if($(this).parent().hasClass('md-input-wrap')){
+        if($(this).val()!==''){
+            $(this).addClass('md-hastext');
+            $(this).parent('.md-input-wrap').addClass('md-hastext');
+        }else{
+            $(this).removeClass('md-hastext');
+            $(this).parent('md-input-wrap').removeClass('md-hastext');
+        }
+    }
+});
+input.on('focus',function(){
+    if($(this).parent().hasClass('md-input-wrap')){
+        $(this).parent('.md-input-wrap').addClass('md-hastext');
+    }
+});
+
+document.querySelector('label').addEventListener('click', function(){
+    const input = document.getElementById('username');
+    input.focus();
+});
